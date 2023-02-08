ff9846c8ebf26317299706cdf9063db7b0328053
4st-pho <dangvantu2k2@gmail.com>
2023-02-08 11:06:40 +0700

    fix some error

--- lib/bloc/addtodo_bloc.dart
+++ lib/bloc/addtodo_bloc.dart
@@ -1,28 +1,37 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 
 import '../model/todo.dart';
 
-class AddTodoBloc {
-  StreamController<List<String>> _streamListTodo =
-      StreamController<List<String>>();
+class AddTodoBloc with WidgetsBindingObserver {
+  late final StreamSubscription<List<Todo>> _totoListener;
+  AddTodoBloc(){
+    WidgetsBinding.instance.addObserver(this);
+    _totoListener = readAllTodo().listen((event) {
+      print('**********'*30);
+                print('rebuild');
+      _streamListTodo.add( event);
+      
+    });
+  }
+  final StreamController<List<Todo>> _streamListTodo =
+      StreamController<List<Todo>>();
 
-  Stream<List<String>> get listTodoStream => _streamListTodo.stream;
+  Stream<List<Todo>> get listTodoStream => _streamListTodo.stream;
   final docTodo = FirebaseFirestore.instance.collection('todo');
 
-  Stream<QuerySnapshot> readAllTodo() {
+  Stream<List<Todo>> readAllTodo() {
     final todoSnapshot = FirebaseFirestore.instance
         .collection('todo')
         .orderBy('current_date', descending: true)
         .snapshots();
-    return todoSnapshot;
+    return todoSnapshot.map(
+        (event) => event.docs.map((e) => Todo.fromJson(e.data())).toList());
   }
 
-  void addTodoList(String todo) {
-    todos.add(todo);
-    _streamListTodo.sink.add(todos);
-  }
+
 
   Future<void> addTodo(Todo content) async {
     final docTodo = FirebaseFirestore.instance.collection('todo').doc();
@@ -49,8 +58,29 @@ class AddTodoBloc {
         .catchError((e) => print(e));
   }
 
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    switch (state) {
+      case AppLifecycleState.resumed:
+      print('resumed');
+      _totoListener.resume();
+        break;
+      case AppLifecycleState.inactive:
+        break;
+      case AppLifecycleState.paused:
+      print('pause');
+      _totoListener.pause();
+        break;
+      case AppLifecycleState.detached:
+        break;
+    }
+  }
+
   void dispose() {
+    _totoListener.pause();
+    _totoListener.cancel();
     _streamListTodo.close();
+    WidgetsBinding.instance.removeObserver(this);
   }
 }
 
